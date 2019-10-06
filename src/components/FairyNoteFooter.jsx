85c66172bd82c0c371444ef13fcb146ac0d928bd
miuzel <miuzel@gmail.com>
2019-10-06 23:35:55 +0800

    bug fix live stream

--- src/components/FairyNoteFooter.jsx
+++ src/components/FairyNoteFooter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FairyNoteFooter = () => (
     <div id="extensionfooter">
-        © 2019 FairyNote  -  Version 2.4.1
+        © 2019 FairyNote  -  Version 2.4.2
     </div>
 )
 
