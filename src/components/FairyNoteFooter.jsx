a604ae51c2e9cd571c4182ecc2adb130cf32f645
miuzel <mingxuan.zhang@aseit.cn>
2019-11-04 03:32:14 +0800

    bug fixes

--- src/components/FairyNoteFooter.jsx
+++ src/components/FairyNoteFooter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FairyNoteFooter = () => (
     <div id="extensionfooter">
-        © 2019 FairyNote  -  Version 2.6.2
+        © 2019 FairyNote  -  Version 2.6.3
     </div>
 )
 
