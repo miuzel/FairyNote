3592dee0f3f648b29133281f8a9b0d0f31b31bf5
miuzel <miuzel@gmail.com>
2019-11-04 03:32:14 +0800

    bug fixes

--- src/components/inputs/MyTextArea.jsx
+++ src/components/inputs/MyTextArea.jsx
@@ -2,20 +2,31 @@ import React, { Component } from 'react'
 import { Input } from 'antd'
 const { TextArea } = Input
 export default class MyTextArea extends Component {
+    state = {
+      value: this.props.value,
+      dirty: false
+    }
     textArea = React.createRef()
     handleTextChange(e){
       let value = ""
       if(e && e.target && typeof(e.target.value) !== "undefined"){
-        value =e.target.value
+        value = e.target.value
       }
-      this.props.onChange(value)
+      this.setState({value,dirty:true})
+    }
+    handleBlur(){
+      if(this.state.dirty){
+        this.setState({dirty:false})
+        this.props.onChange(this.state.value)
+      }
     }
     render() {
         return (
             <TextArea placeholder={this.props.placeholder}
                 ref={this.textArea}
-                value={this.props.value}
+                value={this.state.value}
                 onFocus={this.props.onFocus}
+                onBlur={this.handleBlur.bind(this)}
                 size="small" 
                 className = {this.props.className}
                 // some how this function will miss this object so 
