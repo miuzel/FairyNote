aa58f6b60b5cc29b6af63eb1937e14ddd8fc3227
miuzel <miuzel@gmail.com>
2019-11-14 12:38:00 +0800

    auto change version numbers

--- scripts/builddev.js
+++ scripts/builddev.js
@@ -1,5 +1,6 @@
 'use strict';
-
+const packageConfig = require('../package');
+const replace = require('replace-in-file');
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
@@ -133,6 +134,17 @@ function copyPublicFolder() {
     dereference: true,
     filter: file => file !== paths.appHtml,
   });
+  try {
+    let changedFiles = replace.sync({
+      files: paths.appBuild+"/manifest.json",
+      from: "--VERSION--",
+      to: packageConfig.version
+    });
+    console.log('Modified files:', changedFiles.map(x=>x.file).join(', '));
+  }
+  catch (error) {
+    console.error('Error occurred:', error);
+  }
 }
 function copyLocaleFolder() {
   fs.copySync(paths.locale, paths.applocale, {
